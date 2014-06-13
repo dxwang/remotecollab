4ff3723dd64ce396d66257464e8c6add7c486f51
Ubuntu <ubuntu@ip-172-31-17-173.ec2.internal>
2014-06-13 00:53:10 +0000

    included mongoose, with kitten example

--- index.js
+++ index.js
@@ -2,9 +2,41 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+    var kittySchema = mongoose.Schema({
+	name: String
+    })
+    kittySchema.methods.speak = function() {
+	var greeting = this.name
+	? "Meow name is " + this.name
+	: "I don't have a name"
+   	console.log(greeting);
+    }
+
+    var Kitten = mongoose.model('Kitten', kittySchema)
+    var silence = new Kitten({ name: 'Silence' })
+    console.log(silence.name)
+    var fluffy = new Kitten({ name: 'fluffy' });
+    fluffy.speak()
+    fluffy.save(function (err, fluffy) { 
+	if (err) return console.error(err);
+	fluffy.speak();
+    });
+
+    Kitten.find(function (err, kittens) { 
+	if (err) return console.error(err);
+	console.log(kittens)
+    })
+});
 
 var insertLine = function() {
-    console.log("Vithushan");
+  //insert the line 0,0 100,100
+  
 };
 
 app.get('/', function(req, res){
