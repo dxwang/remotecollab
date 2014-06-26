5f0e86189a4f9f2907cc41618bead9ab4ca425af
David Wang <dxw4ng@gmail.com>
2014-06-26 18:49:19 -0400

    Added socket events

--- js/whiteboardModel.js
+++ js/whiteboardModel.js
@@ -5,6 +5,8 @@ window.whiteboardModel = {
 	currentLineId: null,
 	lines: null,
 	whiteboardId: null,
+	userId: null,
+	socket: null,
 
 	/*
 	Mock API Calls
@@ -32,27 +34,50 @@ window.whiteboardModel = {
 	*/
 
 	init: function(){
-		// API call to get lines and possibly whiteboardId
-		if (this.whiteboardId === null){
-			whiteboardModel.mockGetNewWhiteboardId();
-		}
-		whiteboardModel.mockGetLines();
+		// Logic to get whiteboard Id from URL
+		this.whiteboardId = 1;
+		this.socket = io.connect('localhost:3000');
+		this.socket.emit('new user', { whiteboardId: this.whiteboardId});
+		this.socketListeners();
+	},
+
+	socketListeners: function(){
+		this.socket.on('you joined', function(data){
+			this.whiteboardId = data.whiteboardId;
+		});
+
+		this.socket.on('sync', function(data){
+			for (var i=0;i<data.length;i++){
+				var line = data[i];
+				whiteboardModel.updateLine(line.id, line.data);
+			}
+		});
+
+		this.socket.on('draw', function(data){
+			updateLine(data.line.id, data.line.data);
+		});
+
+		this.socket.on('line added', function(data){
+			this.currentLineId = data.id;
+		});
 	},
 
 	updateLine: function(id, line){
-		lines[id] = line;
+		this.lines[id] = line;
 		whiteboardView.draw(line);
 	},
 
 	addPointToCurrentLine: function(coordinates){
-		if (this.currentLineId === null){
-			// API Call to get an Id for the current line
-			whiteboardModel.mockGetNewLineId();
-		}
-		this.currentLine.push(coordinates);
-		whiteboardModel.updateLine(this.currentLineId, this.currentLine);
+		// if (this.currentLineId === null){
+		// 	// API Call to get an Id for the current line
+		// 	whiteboardModel.mockGetNewLineId();
+		// }
+		this.currentLine.push({x: coordinates[0], y: coordinates[1]});
+
+		this.socket.emit('draw', {line: {color: 'black'}, data: this.currentLine});
+		// whiteboardModel.updateLine(this.currentLineId, this.currentLine);
 		// API call to submit line, with line id
-		whiteboardModel.mockAddLine(this.currentLineId, this.currentLine);
+		// whiteboardModel.mockAddLine(this.currentLineId, this.currentLine);
 	},
 
 	endCurrentLine: function(){
